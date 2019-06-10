0cdf83204964f0419664c7f1182e0f178a58cd08
Allan Kamau <akamau41@gmail.com>
2019-06-10 06:41:53 +0300

    add both client Id and Client secret tokens

---
+++ src/app/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+// import { HttpClient, Headers } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+// import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  public username:string;
+  public clientid = 'fb81443e46e9903bfcdb';
+  public clientsecret ='b23d33e16ffc10ce0f4ecfc0903473e22a99e497';
+
+  constructor(private http: HttpClient) { 
+    console.log("sevice is wotking ");
+    this.username = '';
+  }
+
+}
